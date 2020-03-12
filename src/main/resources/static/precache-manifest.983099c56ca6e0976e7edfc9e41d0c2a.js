f1394d98c7f6bd5872a1be7735ed332451dd831e
Murphy <haihuynhng@gmail.com>
2020-03-12 17:48:43 +0700

    add react-app

---
+++ src/main/resources/static/precache-manifest.983099c56ca6e0976e7edfc9e41d0c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee73ca3e9720b65e1708a0949b96ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "380de0d53466d9389210",
+    "url": "/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "60b7d9edf07cc4c380c8",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "380de0d53466d9389210",
+    "url": "/static/js/2.a71bf862.chunk.js"
+  },
+  {
+    "revision": "46ba0170d1075e215aa5dadac597f305",
+    "url": "/static/js/2.a71bf862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b7d9edf07cc4c380c8",
+    "url": "/static/js/main.1dee76b0.chunk.js"
+  },
+  {
+    "revision": "e66a299163181f070308",
+    "url": "/static/js/runtime-main.10415b89.js"
+  }
+]);
