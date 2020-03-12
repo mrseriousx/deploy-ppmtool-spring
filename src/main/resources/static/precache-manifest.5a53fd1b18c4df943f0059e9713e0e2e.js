5ce33b9ce5891f8ffdc67e7d24d9c8fd0388a46b
Murphy <haihuynhng@gmail.com>
2020-03-12 19:14:27 +0700

    fix1.2

---
+++ src/main/resources/static/precache-manifest.5a53fd1b18c4df943f0059e9713e0e2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a933d49914ea570f8ce14ec43cb11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "380de0d53466d9389210",
+    "url": "/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "6764431cb47fde09c635",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "380de0d53466d9389210",
+    "url": "/static/js/2.a71bf862.chunk.js"
+  },
+  {
+    "revision": "46ba0170d1075e215aa5dadac597f305",
+    "url": "/static/js/2.a71bf862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6764431cb47fde09c635",
+    "url": "/static/js/main.90156d57.chunk.js"
+  },
+  {
+    "revision": "e66a299163181f070308",
+    "url": "/static/js/runtime-main.10415b89.js"
+  }
+]);
